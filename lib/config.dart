10a985ba73094b592927a5335bc77582920ce44d
g4rty <64538667+g4rty@users.noreply.github.com>
2025-10-29 21:23:20 +0700

    fix pubspec verison of f1_chart

--- lib/config.dart
+++ lib/config.dart
@@ -6,6 +6,11 @@
 //   // static const String baseUrl = 'http://192.168.1.100:3000';
 //   static const String baseUrl = 'http://10.0.0.74:3000';
 // }
+// class AppConfig {
+//   static const String baseUrl = 'http://192.168.1.100:3000';
+// }
+//
+//
 class AppConfig {
-  static const String baseUrl = 'http://192.168.1.100:3000';
+  static const String baseUrl = 'http://localhost:3000';
 }
