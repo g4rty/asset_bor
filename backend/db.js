c8b6d3a7a2ca381c0163b9c1d967c283407a1c57
g4rty <64538667+g4rty@users.noreply.github.com>
2025-10-30 17:22:54 +0700

    change dir of backend

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'asset_borrowing'
-});
-
-module.exports = connection;
