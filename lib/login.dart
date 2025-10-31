e98fe0c59e0de8926b56f3742779403cf8713af0
g4rty <64538667+g4rty@users.noreply.github.com>
2025-10-31 11:45:04 +0700

    update login lecturer to redict to dashboard

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 
-import 'package:asset_bor/lecturer/lecturer_asset_list.dart';
-
 import '/config.dart';
 import 'auth_storage.dart';
 import '/lecturer/lecturer_home_page.dart';
@@ -62,7 +60,7 @@ class _LoginPageState extends State<LoginPage> {
 
         Widget? destination;
         if (_role == 'lecturer') {
-          destination = const LecturerAssetList();
+          destination = const LecturerHomePage();
         } else if (_role == 'staff') {
           destination = const StaffHomePage();
         } else if (_role == 'student') {
